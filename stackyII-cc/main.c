487c73025a4d706fc95daa77deb7030e168fdba6
C-Elegans <mtnolan2640@gmail.com>
2016-03-29 11:34:52 -0400

    Initial Commit

---
+++ stackyII-cc/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  stackyII-cc
+//
+//  Created by Michael Nolan on 3/29/16.
+//  Copyright © 2016 Michael Nolan. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	printf("Hello, World!\n");
+    return 0;
+}
